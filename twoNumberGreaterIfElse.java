970a7dd151e7379aba5d3c76dbebec1d28221f97
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-03-27 16:29:35 +0800

    Create twoNumberGreaterIfElse.java

---
+++ twoNumberGreaterIfElse.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        
+        /*
+
+        Write a program that asks the user to enter two numbers and tells the user which one is larger using if-else statements only.
+
+        Sample Output 1
+
+        Enter first number: 7
+        Enter second number: 6
+        7 is larger
+
+        Sample Output 2
+
+        Enter first number: 1
+        Enter second number: 3
+        3 is larger
+
+        */
+      
+        Scanner sc = new Scanner(System.in);
+        int num1,num2;
+
+        System.out.print("Enter first number: ");
+        num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        num2 = sc.nextInt();
+
+        if(num1 > num2){
+            System.out.print(num1 + " is larger");
+        }
+        else{
+            System.out.print(num2 + " is larger");
+        }
+        
+    }
+}
