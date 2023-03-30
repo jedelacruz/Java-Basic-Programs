78d42e2e097962b02574c64997d86381f22338c0
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-03-30 17:10:04 +0800

    Create swap2Variables.java

---
+++ swap2Variables.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        
+        /*
+
+        Write a program that swaps the values of two variables using the assignment operator.
+
+        Sample Output 1
+
+        Enter value a: 5
+        Enter value b: 10
+        Before swap a = 5, b = 10
+        After swap a = 10, b = 5
+
+        */
+      
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;
+
+        System.out.print("Enter value a: ");
+        a = sc.nextInt();
+
+        System.out.print("Enter value b: ");
+        b = sc.nextInt();
+
+        System.out.println("Before swap a = " + a + ", b = " + b);
+
+        c = a;
+        a = b;
+        b = c;
+
+        System.out.print("After swap a = " + a + ", b = " + b);
+
+        
+    }
+}
