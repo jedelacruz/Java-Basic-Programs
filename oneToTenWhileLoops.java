d054ad6ea10eee517dad096edf793f3c1294c3b9
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-04-01 09:13:50 +0800

    Create oneToTenWhileLoops.java

---
+++ oneToTenWhileLoops.java
@@ -0,0 +1,18 @@
+package competition;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+    	
+        Scanner sc = new Scanner(System.in);
+        
+        int ct = 0;
+        
+        while(ct <= 10) {
+        	System.out.println(ct);
+        	ct++;
+        }
+        
+    }
+}
