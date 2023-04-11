c0f7ab9f9583a3c74934119d78b2457bffe1324d
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-04-11 16:44:32 +0800

    Create longestWordChecker.java

---
+++ longestWordChecker.java
@@ -0,0 +1,21 @@
+package competition;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+    	
+    	  Scanner sc = new Scanner(System.in);
+          System.out.print("Enter a list of words separated by spaces: ");
+          String input = sc.nextLine();
+          String[] words = input.split(" ");
+          String longest = words[0];
+          for (int i = 1; i < words.length; i++) {
+              if (words[i].length() > longest.length()) {
+                  longest = words[i];
+              }
+          }
+          System.out.println("Longest word: " + longest);
+       
+    }
+}
